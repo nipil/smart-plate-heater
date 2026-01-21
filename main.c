ecc4affa8992efd1635fc4c1b2b9b518acfa72ef
nipil <nipil@users.noreply.github.com>
2026-01-21 08:35:06 +0100

    First stab at codig the conversion workflow

--- main.c
+++ main.c
@@ -20,25 +20,6 @@
 // https://support.microchip.com/s/article/Delay-functions-in-XC16---XC8-using-PIC-devices
 #define _XTAL_FREQ 4000000L
 
-void delay_cycles(uint8_t count) {
-    // in simulator, triggers a cycle delay of :
-    // 1=20cyc, 2=24cyc, 3=32cyc, 40cyc, 48cyc ... +8cyc/unit
-    while (count--) {
-    };
-}
-
-void wait_for_data_high() {
-    while (1) {
-        if (GPIObits.GP2)
-            return;
-    }
-}
-
-void wait_for_data_low() {
-    // post-delay of about 10 cycles
-    while (GPIObits.GP2);
-}
-
 // non repeating pattern for stimulus
 // 11111011 10011010 11000101
 
@@ -49,37 +30,75 @@ void wait_for_data_low() {
 // GP3 / input / button (not debounced)
 
 // HX711 : after reset channel is A with gain 128
-
-
-#define WAIT_FOR_LOW(X) while(X)
+#define HX711_N_BITS 24
 
 void main(void) {
-    // init
-    OPTION = 0xFF & ~T0CS; // Allow using GP2 as GPIO (disable T0CS)
-    TRIS = 0b1100; // use GP3 and GP2 as input, GP1 and GP0 as output
-    GPIObits.GP0 = 0; // disable optotriac
-    GPIObits.GP1 = 1; // disable HX711
+    // Allow using GP2 as GPIO (disable T0CS)
+    OPTION = 0xFF & ~T0CS;
+    // use GP3 and GP2 as input, GP1 and GP0 as output
+    TRIS = 0b1100;
+    // disable optotriac
+    GPIObits.GP0 = 0;
+    // disable HX711
+    GPIObits.GP1 = 1;
+    // make sure HX711 goes to sleep
+    _delay(100);
 
-    _delay(100); // make sure HX711 goes to sleep
-    GPIObits.GP1 = 0; // activate HX711
-//    WAIT_FOR_LOW(GPIObits.GP2); // conversion not ready while DOUT is HIGH
-    wait_for_data_low(); // conversion not ready while DOUT is HIGH
-    GPIObits.GP1 = 1; // disable HX711
+    // make sure HX711 goes to sleep (>60us) at first
+    while (1) {
 
+        // DEBUG: time marker
+        GPIObits.GP0 = 1;
 
-    // when PD_SCK is LOW, chip is active
-    // when DOUT falls, data is ready
-    // wait for T1 (+0.1 micros) before PD_SCK rises
-    // wait for T2 (+0.1 micros) before reading DOUT
-    // PD_SCK should stay HIGH for +0.2 to less than 50 micros
-    // PD_SCK should go LOW for +0.2 micros
-    // PD_SCK should do 24 cycles (data MSB first)
-    // output is a 2's complement value, from min 0x800000 to max 0x7FFFFF
-    // PD_SCK should do 1-3 more cycle for next channel select
-    // 1 = channel A gain 128, 2 = channel B gain 32, 3 = channel A gain 64
-    // chip powers down after 60 micros of PD_SCK being HIGH
+        // HX711 output is a 24 bits **signed** int
+        // output is a 2's complement value
+        // from min 0x800000 to max 0x7FFFFF
+        int32_t value = 0;
 
+        // when PD_SCK is LOW, chip is active
+        GPIObits.GP1 = 0;
 
+        // wait for data ready (T1)
+        while (GPIObits.GP2);
 
-    uint8_t data = GPIO;
+        // pull bits MSB first
+        for (uint8_t i = 0; i < HX711_N_BITS; i++) {
+            // clock rise
+            GPIObits.GP1 = 1;
+            // wait for data ready (T2)
+            _delay(1);
+            // read data bit and append from low side
+            value = (value << 1) | GPIObits.GP2;
+            // wait high (T3)
+            _delay(1);
+            // clock fall
+            GPIObits.GP1 = 0;
+            // wait low (T4)
+            _delay(1);
+        }
+
+        // PD_SCK should do 1-3 more cycle for next channel select
+        // 1 = channel A gain 128
+        // 2 = channel B gain 32
+        // 3 = channel A gain 64
+
+        // clock rise
+        GPIObits.GP1 = 1;
+        // wait high (T3)
+        _delay(1);
+        // clock fall
+        GPIObits.GP1 = 0;
+        // wait low (T4)
+        _delay(1);
+
+        // disable HX711
+        GPIObits.GP1 = 1;
+
+        // DEBUG: time marker
+        GPIObits.GP0 = 0;
+        _delay(1);
+
+        // make sure HX711 goes to sleep
+        _delay(100);
+    }
 }
